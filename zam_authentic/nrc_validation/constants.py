98777def8ac5fa6dc33836a6ac89b03a1972418a
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-03-15 13:20:40 +0200

    Added allowed delimiters.

--- zam_authentic/nrc_validation/constants.py
+++ zam_authentic/nrc_validation/constants.py
@@ -1,2 +1,2 @@
 NRC_PATTERN = r'^\d\dp\d\dp\d\dp\d\d\p\d$'
-
+ALLOWED_DELIMITERS = ['-', '_', '/', None]
--- zam_authentic/nrc_validation/utils.py
+++ zam_authentic/nrc_validation/utils.py
@@ -1,5 +1,7 @@
 from constants import NRC_PATTERN
 
-
-def create_regex(delimiter):
+### function to modify regex pattern based on delimiter passed .
+def create_regex(delimiter:str)->str:
     return NRC_PATTERN.replace('p', 'delimiter')
+def validate_delimiter(delimiter:str)->str:
+    return
