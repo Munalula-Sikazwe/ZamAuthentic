a205733e5a296b93134167bb801f33b3a1f033a7
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-03-15 15:09:53 +0200

    created nrc valid pattern checker.

--- zam_authentic/nrc_validation/utils.py
+++ zam_authentic/nrc_validation/utils.py
@@ -1,3 +1,5 @@
+from re import match
+
 from constants import NRC_PATTERN, ALLOWED_DELIMITERS
 
 
@@ -10,3 +12,8 @@ def create_nrc_regex(delimiter: str) -> str:
 
 def validate_delimiter(delimiter: str) -> bool:
     return delimiter in ALLOWED_DELIMITERS
+
+
+# function checks if  nrc is valid with the given delimiter.
+def is_valid_nrc(nrc_number: str, nrc_pattern: str):
+    return bool(match(nrc_pattern, nrc_number))
--- zam_authentic/nrc_validation/validate.py
+++ zam_authentic/nrc_validation/validate.py
@@ -1,13 +1,16 @@
 from constants import ALLOWED_DELIMITERS
-from utils import validate_delimiter, create_nrc_regex
+from utils import validate_delimiter, create_nrc_regex, is_valid_nrc
 
 
 # A function to check if the nrc is valid.
 def validate_nrc(nrc_number: str, delimiter: str = None) -> bool:
     is_valid = validate_delimiter(delimiter)
+    if delimiter is None:
+        return len(nrc_number) == 9
     if not is_valid:
         raise Exception(f'NRC delimiter is invalid! values should be seperated by {ALLOWED_DELIMITERS}')
     nrc_pattern = create_nrc_regex(delimiter)
+    return is_valid_nrc(nrc_number, nrc_pattern)
 
 
 validate_nrc('sdadfa', delimiter='-')
