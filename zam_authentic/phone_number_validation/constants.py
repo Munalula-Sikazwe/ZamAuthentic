3160c2fc163f71371deae12f6ce4b1518a3adea4
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-03-16 20:24:04 +0200

    added some minor formatting.

--- zam_authentic/phone_number_validation/constants.py
+++ zam_authentic/phone_number_validation/constants.py
@@ -1,4 +1,9 @@
-# carriers that available in Zambia. Note that this list may change over time as it is affected by external factors like government polices concerning carriers.
+"""
+ carriers that available in Zambia.
+ Note that this list may change over time.
+ As it is affected by external factors like government polices concerning carriers.
+"""
+
 CARRIERS = {
     "96": "MTN",
     "97": "Airtel",
