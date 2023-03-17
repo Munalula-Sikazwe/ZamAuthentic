92eca3973351605f5b921fea667ba5df3153ceb0
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-03-17 14:03:01 +0200

    refactored the locations.

---
+++ zam_authentic/locations/districts.py
@@ -0,0 +1,15 @@
+DISTRICTS = {
+    'Central Province': ['Chibombo', 'Kabwe', 'Kapiri Mposhi', 'Mkushi', 'Mumbwa', 'Serenje'],
+    'Copperbelt Province': ['Chililabombwe', 'Chingola', 'Kalulushi', 'Kitwe', 'Luanshya', 'Lufwanyama', 'Masaiti',
+                            'Mpongwe', 'Mufulira', 'Ndola'],
+    'Eastern Province': ['Chadiza', 'Chipata', 'Katete', 'Lundazi', 'Mambwe', 'Nyimba', 'Petauke'],
+    'Luapula Province': ['Chienge', 'Chiengi', 'Kawambwa', 'Mansa', 'Milenge', 'Mwansabombwe', 'Nchelenge', 'Samfya'],
+    'Lusaka Province': ['Chilanga', 'Chirundu', 'Kafue', 'Luangwa', 'Lusaka', 'Mumbwa'],
+    'Muchinga Province': ['Chama', 'Chinsali', 'Isoka', 'Mpika', 'Mafinga', 'Nakonde'],
+    'Northern Province': ['Kasama', 'Luwingu', 'Mporokoso', 'Mpulungu', 'Mbala', 'Nchelenge'],
+    'North-Western Province': ['Chavuma', 'Kabompo', 'Kasempa', 'Manyinga', 'Mufumbwe', 'Mwinilunga', 'Solwezi',
+                               'Zambezi'],
+    'Southern Province': ['Chikankata', 'Choma', 'Gwembe', 'Kalomo', 'Kazungula', 'Livingstone', 'Mazabuka', 'Monze',
+                          'Namwala', 'Pemba', 'Siavonga'],
+    'Western Province': ['Kalabo', 'Kaoma', 'Lukulu', 'Mongu', 'Senanga', 'Sesheke', 'Shangombo']
+}
---
+++ zam_authentic/locations/provinces.py
@@ -0,0 +1,3 @@
+PROVINCES = ['Central Province', 'Copperbelt Province', 'Eastern Province', 'Luapula Province', 'Lusaka Province',
+             'Muchinga Province', 'Northern Province', 'North-Western Province', 'Southern Province',
+             'Western Province']
