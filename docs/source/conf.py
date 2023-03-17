6a9b1d048239d853182c599256a47ed8a51446ac
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-03-17 22:57:19 +0200

    changed master doc from contents to implied default

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,6 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-master_doc = 'contents'
+
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
